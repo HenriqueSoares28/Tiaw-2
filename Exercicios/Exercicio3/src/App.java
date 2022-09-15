73437cc7d4f8ce5474ca019bd2e7c13ec98a4e5b
Henrique Soares <henrique.soares.2811@gmail.com>
2022-09-15 15:41:24 -0300

    exercicio 3

---
+++ Exercicios/Exercicio3/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+    //sum 2 numbers 
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
